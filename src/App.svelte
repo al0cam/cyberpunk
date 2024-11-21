0bd2e982b77211de8b9e0595d90e595f6df152cc
Al0cam <benjaminsikac@gmail.com>
2024-11-21 20:33:29 +0100

    working on marquee effect for scrolling text

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 </script>
 
-<main class="m-0 h-screen object-contain">
-  <span class="static bottom-0 right-0 -rotate-45 text-6xl ">Wake the fuck up, Samurai. We have a city to burn.</span>
+<main class="m-0 h-screen object-contain relative overflow-x-hidden">
+  <div class="flex flex-row">
+    <div class=" animate-marquee text-6xl whitespace-nowrap">
+      Wake the fuck up, Samurai. We have a city to burn.
+    </div>
+    <div class=" animate-marquee text-6xl whitespace-nowrap">
+      Wake the fuck up, Samurai. We have a city to burn.
+    </div>
+  </div>
+  <span
+    class="absolute bottom-0 left-0 rotate-45 text-6xl w-64 h-14 overflow-clip"
+    >Wake the fuck up, Samurai. We have a city to burn.</span
+  >
+  <span class="absolute bottom-0 right-0 -rotate-45 text-6xl w-64"
+    >LAPD > DO NOT CROSS</span
+  >
 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,22 @@ export default {
       "error": "#ff5484",
       "error-content": "#160206",
     },
-    extend: {},
+    extend: {
+      animation:{
+        'marquee': 'marquee 5s linear infinite',
+        'marquee2': 'marquee2 5s linear infinite',
+      },
+      keyframes:{
+        marquee: {
+          '0%': {transform: 'translateX(0%)'},
+          '100%': {transform: 'translateX(-100%)'}
+        },
+        marquee2: {
+          '0%': {transform: 'translateX(100%)'},
+          '100%': {transform: 'translateX(0%)'}
+        }
+      }
+    },
   },
   plugins: [
     require('daisyui'),
