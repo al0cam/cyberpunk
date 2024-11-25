fa7b0587045b75ed232949ec5d31dfb37ffa290a
Al0cam <benjaminsikac@gmail.com>
2024-11-25 23:30:18 +0100

    added marquee to side elements and extracted it into a separate component

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
+  import Marquee from "./components/Marquee.svelte";
+    import JohnnySilverhand from './assets/Johnny.jpg'
+  let activeImage = JohnnySilverhand;
 </script>
 
-<main class="m-0 h-screen object-contain relative overflow-x-hidden">
-  <div class="flex flex-row">
-    <div class=" animate-marquee text-6xl whitespace-nowrap">
-      Wake the fuck up, Samurai. We have a city to burn.
-    </div>
-    <div class=" animate-marquee text-6xl whitespace-nowrap">
-      Wake the fuck up, Samurai. We have a city to burn.
-    </div>
-  </div>
-  <span
-    class="absolute bottom-0 left-0 rotate-45 text-6xl w-64 h-14 overflow-clip"
-    >Wake the fuck up, Samurai. We have a city to burn.</span
-  >
-  <span class="absolute bottom-0 right-0 -rotate-45 text-6xl w-64"
-    >LAPD > DO NOT CROSS</span
-  >
+<main class="m-0 h-screen object-contain relative overflow-hidden">
+  <Marquee
+    class="absolute bottom-[10vw] -left-[10vw] rotate-45 text-6xl w-[40vw] text-yellow-300"
+
+    text="Wake the fuck up, Samurai. We have a city to burn."
+  />
+  <Marquee
+    class="absolute bottom-0 -right-[3vw] -rotate-45 text-6xl w-[20vw] text-yellow-300"
+    text="LAPD > DO NOT CROSS"
+  />
+
+  <img
+    class="absolute bg-cover bg-center top-0 left-0 z-0 opacity-20 object-cover object-top h-screen w-screen"
+    src={activeImage}
+    alt="Johnny Silverhand"
+  />
 </main>
---
+++ src/components/Marquee.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let { text = "", class: _class = "" } = $props();
+</script>
+
+<div class={_class}>
+  <div class="flex flex-row overflow-x-hidden gap-2">
+    <div class=" animate-marquee text-6xl whitespace-nowrap">
+      {text}
+    </div>
+    <div class=" animate-marquee text-6xl whitespace-nowrap">
+      {text}
+    </div>
+  </div>
+</div>
