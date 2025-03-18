92bfd9899a85f073e528368963786d99dd8ea5bc
Al0cam <benjaminsikac@gmail.com>
2025-03-18 20:32:18 +0100

    added new images and onmouseover event

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,25 @@
   import JohnnySilverhand from "./assets/Johnny.jpg";
   import cyberpunk_logo from "./assets/cyberpunk_logo_cropped.png";
   import samurai_cropped from "./assets/samurai_cropped.png";
+  import samurai_background from "./assets/samurai_background.jpg";
+  import cyberpunk_background from "./assets/cyberpunk_background.jpg";
   import Marquee from "./components/Marquee.svelte";
   import ReverseMarquee from "./components/ReverseMarquee.svelte";
+
   const activeImage = JohnnySilverhand;
 
+  let background = null;
 
   // TODO: use for animations
   let leftWidth = 50;
   let rightWidth = 50;
+
+  function changeBackground(newBackground: string) {
+    background = newBackground;
+  }
+  function resetBackground() {
+    background = null;
+  }
 </script>
 
 <main class="m-0 h-screen w-screen object-contain relative overflow-hidden">
@@ -27,12 +38,22 @@
     text="LAPD > DO NOT CROSS"
   />
 
+  <img
+    class="absolute top-0 left-0 z-10 object-cover object-top h-screen w-screen"
+    src={background}
+    alt="A background which changes based on the item hovered"
+  />
+
   <div class="absolute z-30 w-full h-full flex flex-row items-center">
-    <div class="z-30 w-1/2 overflow-hidden ">
+    <div class="z-30 w-1/2 overflow-hidden">
       <img
         class=" w-svw max-w-none"
         src={cyberpunk_logo}
         alt="Cyberpunk 2077"
+        onmouseover={() => changeBackground(cyberpunk_background)}
+        onfocus={() => changeBackground(cyberpunk_background)}
+        onmouseleave={() => resetBackground()}
+        onblur={() => resetBackground()}
       />
     </div>
 
